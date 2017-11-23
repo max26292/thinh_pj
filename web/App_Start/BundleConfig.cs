1d32268971995c1c1653bfb7c416f04052db4caa
max26292 <max26292@yahoo.com>
2017-11-23 09:53:03 +0700

    add controller, view, css

--- web/App_Start/BundleConfig.cs
+++ web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/StyleSheet.css"));
         }
     }
 }
---
+++ web/Controllers/MyProjectController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class MyProjectController : Controller
+    {
+        // GET: MyProject
+        public ActionResult Index()
+        {
+            return View("Home");
+
+        }
+        public ActionResult boncho()
+        {
+            return View();
+
+        }
+        public ActionResult baycho()
+        {
+            return View();
+
+        }
+        public ActionResult muoisaucho()
+        {
+            return View();
+
+        }
+        public ActionResult haichincho()
+        {
+            return View();
+
+        }
+        public ActionResult nammuoicho()
+        {
+            return View();
+
+        }
+        public ActionResult news()
+        {
+            return View();
+
+        }
+    }
+}
